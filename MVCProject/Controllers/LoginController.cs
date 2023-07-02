2709682760ebe6e499f6d13a5853a868b1022b66
Enes Kurt <eneskurt1996@hotmail.com>
2023-07-02 07:31:45 +0300

    register added.

---
+++ MVCProject/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCProject/Controllers/StoragesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class StoragesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
